d8e4fa64767ff13fc611380c876df3c08850a63a
Navid <50656059+Navid-Z@users.noreply.github.com>
2022-06-03 15:47:02 +0430

    Add files via upload

---
+++ pythonNodes/number_subscriber.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+''''
+
+Author: Navid Zarrabi
+Date: 06/03/2022
+Topic: ROS workshop
+Name: First Topic (Subscriber)
+
+'''
+
+import rospy
+from std_msgs.msg import Int64
+
+
+def callback_number(msg):
+	rospy.loginfo(msg.data)
+
+
+if __name__ == '__main__':
+	rospy.init_node('number_subscriber')
+	sub = rospy.Subscriber("/number", Int64, callback_number)
+	rospy.spin()
